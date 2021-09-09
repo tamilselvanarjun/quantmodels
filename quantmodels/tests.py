375d498dcb0f70406a23a74fa6afb2a44458fa4d
Jair3121 <JessGrecco2775@hotmail.com>
2021-09-09 10:18:20 +0500

    Adding comment

--- quantmodels/tests.py
+++ quantmodels/tests.py
@@ -4,10 +4,10 @@ from option_pricing import binomial_option_pricing, black_scholes_option_pricing
 class TestOptionPricing(unittest.TestCase):
 
     def test_binomial_option_pricing(self):
-        # Test call option pricing using binomial model
+        # Test call option pricing using binomial model.
         call_price = binomial_option_pricing(100, 100, 1, 0.05, 0.2, 100, 'call')
         self.assertAlmostEqual(call_price, 10.4506, places=4)
 
-        # Test put option pricing using binomial model
+        # Test put option pricing using binomial model.
         put_price = binomial_option_pricing(100, 100, 1, 0.05, 0.2, 100, 'put')
         self.assertAlmostEqual(put_price, 5.5735, places=4)
