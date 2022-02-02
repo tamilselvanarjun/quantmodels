cd271f80258c06bdb0ec9eb112fdf74a466d81ea
tamilselvanarjun <nishantamil@gmail.com>
2022-02-02 10:18:20 +0500

    updates in code

--- quantmodels/opm.py
+++ quantmodels/opm.py
@@ -16,3 +16,17 @@ def binomial_option_pricing(S, K, T, r, sigma, n, option_type='call'):
             option_values[i][j] = math.exp(-r * dt) * (p * option_values[i + 1][j + 1] + (1 - p) * option_values[i + 1][j])
 
     return option_values[0][0]
+
+# Example Usage
+#if __name__ == "__main__":
+#    underlying_price = 100  # Current price of the underlying asset
+#    strike_price = 100      # Strike price of the option
+#    time_to_maturity = 1    # Time to maturity in years
+#    risk_free_rate = 0.05   # Risk-free interest rate
+#    volatility = 0.2       # Volatility of the underlying asset
+#    periods = 100           # Number of periods in the binomial model
+
+#    call_price = binomial_option_pricing(underlying_price, strike_price, time_to_maturity, risk_free_rate, volatility, periods, 'call')
+#    put_price = binomial_option_pricing(underlying_price, strike_price, time_to_maturity, risk_free_rate, volatility, periods, 'put')
+#    print(f"Call Option Price: {call_price:.2f}")
+#    print(f"Put Option Price: {put_price:.2f}")
