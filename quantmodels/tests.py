381c63fe94d00c88de1dce210c38d871455718d2
wilsonlogan42 <gkkykkjv@hotmail.com>
2023-08-22 10:18:20 +0500

    Fix broken links

--- quantmodels/tests.py
+++ quantmodels/tests.py
@@ -8,6 +8,6 @@ class TestOptionPricing(unittest.TestCase):
         call_price = binomial_option_pricing(100, 100, 1, 0.05, 0.2, 100, 'call')
         self.assertAlmostEqual(call_price, 10.4506, places=4)
 
-        
+        # Test put option pricing using binomial model
         put_price = binomial_option_pricing(100, 100, 1, 0.05, 0.2, 100, 'put')
         self.assertAlmostEqual(put_price, 5.5735, places=4)
