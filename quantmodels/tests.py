1a117dd21157476a7c8e75ed233ef1a26dc8deca
Tamilselvan A <42732673+tamilselvanarjun@users.noreply.github.com>
2023-10-28 00:00:00 +0530

    testing the changes

--- quantmodels/tests.py
+++ quantmodels/tests.py
@@ -20,3 +20,12 @@ class TestOptionPricing(unittest.TestCase):
         # Test put option pricing using Black-Scholes model
         put_price = black_scholes_option_pricing(100, 100, 1, 0.05, 0.2, 'put')
         self.assertAlmostEqual(put_price, 5.5735, places=4)
+        
+    def test_black_scholes_implied_volatility(self):
+        # Test implied volatility calculation using Black-Scholes model for call option
+        implied_volatility_call = black_scholes_implied_volatility(10, 100, 100, 1, 0.05, 'call')
+        self.assertAlmostEqual(implied_volatility_call, 0.2000, places=4)
+
+        # Test implied volatility calculation using Black-Scholes model for put option
+        implied_volatility_put = black_scholes_implied_volatility(10, 100, 100, 1, 0.05, 'put')
+        self.assertAlmostEqual(implied_volatility_put, 0.2000, places=4)
